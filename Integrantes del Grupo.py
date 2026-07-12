1e5cf08064895909fa535a408cc3e7efbcdaaace
chfaundez-ui <ch.faundez@duocuc.cl>
2026-07-12 19:47:03 -0400

    Add files via upload

---
+++ Integrantes del Grupo.py
@@ -0,0 +1,9 @@
+# Script para imprimir integrantes del grupo
+integrantes = [
+    "Christian Faundez",
+    "Bastian Gomez"
+]
+
+print("--- Lista de Integrantes del Grupo Evaluación Final Transversal---")
+for integrante in integrantes:
+    print(f"- {integrante}")
