78b89ef296f469860caf78719e3db578755493dd
chfaundez-ui <ch.faundez@duocuc.cl>
2026-07-12 21:00:16 -0400

    Delete Integrantes del Grupo.py

--- Integrantes del Grupo.py
+++
@@ -1,9 +0,0 @@
-# Script para imprimir integrantes del grupo
-integrantes = [
-    "Christian Faundez",
-    "Bastian Gomez"
-]
-
-print("--- Lista de Integrantes del Grupo Evaluación Final Transversal---")
-for integrante in integrantes:
-    print(f"- {integrante}")
